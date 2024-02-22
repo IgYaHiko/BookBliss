8b4fdf30bd2be0eca0b847c83b24851fc1f614af
Yahiko <subhrokolay2@gmail.com>
2024-02-23 00:43:40 +0530

    initiate project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='w-full h-screen bg-zinc-950 flex items-center justify-center'>
+      <h1 className='text-red-500 text-5xl font-bold'>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
